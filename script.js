82550397fa83908e527292497351f7e3c2c6f39f
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-09-15 23:40:07 -0700

    prime numbers

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 //Part 1
 
-let x = 1;
+// let x = 1;
 
-while (x <= 100) {
-    if (x % 3 === 0 && x % 5 !== 0) {
-        console.log("Fizz");
-    }
-    if (x % 3 !== 0 && x % 5 === 0) {
-        console.log("Buzz");
-    }
-    if (x % 3 === 0 && x % 5 === 0) {
-        console.log("Fizz Buzz")
-    }
-    if (x % 3 !== 0 && x % 5 !== 0) {
-        console.log(x);
-    }
+// while (x <= 100) {
+//     if (x % 3 === 0 && x % 5 !== 0) {
+//         console.log("Fizz");
+//     }
+//     if (x % 3 !== 0 && x % 5 === 0) {
+//         console.log("Buzz");
+//     }
+//     if (x % 3 === 0 && x % 5 === 0) {
+//         console.log("Fizz Buzz")
+//     }
+//     if (x % 3 !== 0 && x % 5 !== 0) {
+//         console.log(x);
+//     }
 
-    x++;
-}
+//     x++;
+// }
 
 //Part 2
 
-let n = 1;
-console.log(n);
+for (let n = 1; n <= 1000; n++) {
+  
 
+  
+    let isPrime = true;
+  
+    for (let j = 2; j < n; j++) {
+      if (n % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+  
+    if (isPrime === true) {
+      console.log(`${n} is the next prime number`);
+    }
+  }
