f01109bb919c139c97017f9dd115aad7040cd48a
MusicalWhiskey <124202980+MusicalWhiskey@users.noreply.github.com>
2024-09-15 23:01:04 -0700

    fixed fizz buzz

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('test')
+let x = 1;
+
+while (x <= 100) {
+    console.log(x);
+    if (x % 3 === 0) {
+        console.log("Fizz");
+    }
+    if (x % 5 === 0) {
+        console.log("Buzz");
+    }
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("Fizz Buzz")
+    }
+
+    x++;
+}
